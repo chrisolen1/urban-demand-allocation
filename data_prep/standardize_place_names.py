aa606436d57d47f75a419f071c1de4b911560ebd
root <root@da-vm.us-central1-a.c.demand-allocation.internal>
2020-08-14 20:16:51 +0000

    another gcp update for data prep

--- data_prep/standardize_place_names.py
+++ data_prep/standardize_place_names.py
@@ -4,14 +4,14 @@ parser.add_argument("--home_directory", action="store", dest="home_directory", t
 parser.add_argument("--geo_directory", action="store", dest="geo_directory", type=str, help="location of the geo shapefiles")
 parser.add_argument("--data_directory", action="store", dest="data_directory", type=str, help="location of the relevant data directory")
 parser.add_argument("--file_name", action="store", dest="file_name", type=str, help="name of the file we're updating")
+parser.add_argument('--gcp', action='store_true', dest='gcp', help='affects whether to configure to running on the cloud')
 
 parse_results = parser.parse_args()
 home_directory = parse_results.home_directory
 geo_directory = parse_results.geo_directory
-res_directory = parse_results.res_directory
-crime_directory = parse_results.crime_directory
-bus_directory = parse_results.bus_directory
+data_directory = parse_results.data_directory
 file_name = parse_results.file_name
+gcp = parse_results.gcp
 
 import sys
 sys.path.append(home_directory)
@@ -20,7 +20,10 @@ import pandas as pd
 import numpy as np
 import json
 from tqdm import tqdm
-import gcsfs
+if gcp:
+    import gcsfs
+    from google.cloud import storage
+    storage_client = storage.Client()
 
 import utilities
 
@@ -30,18 +33,28 @@ are the same across all data sources
 """
 
 # use polygon json files as naming standard
+if gcp:
 
-with open('{}/neighborhood_reformatted.json'.format(geo_directory),'r') as f:
-    neighborhoods = json.load(f)
+    bucket = storage_client.get_bucket(geo_directory[5:])
+    blob = bucket.blob("chicago_neighborhood_reformatted.json")
+    neighborhoods = json.loads(blob.download_as_string(client=None))
+
+    blob = bucket.blob("chicago_tract_reformatted.json")
+    tracts = json.loads(blob.download_as_string(client=None))
+
+else:
     
-with open('{}/tract_reformatted.json'.format(geo_directory),'r') as f:
-    tracts = json.load(f)
+    with open('{}/chicago_neighborhood_reformatted.json'.format(geo_directory),'r') as f:
+        neighborhoods = json.load(f)
+        
+    with open('{}/chicago_tract_reformatted.json'.format(geo_directory),'r') as f:
+        tracts = json.load(f)
 
 # standardize place names in residential data
 
 df = pd.read_csv('{}/{}'.format(data_directory, file_name))
 
-df = list(zip(df.longitude, df.latitude))
+positions = list(zip(df.longitude, df.latitude))
 
 print("matching samples with neighborhoods")
 n = [utilities.point_lookup(neighborhoods, positions[i]) for i in tqdm(range(len(df)))]
@@ -98,5 +111,5 @@ crime['tracts'] = np.array(t)
 
 print("writing crime_standardized.csv")
 crime.to_csv('{}/crime_standardized.csv'.format(data_directory))
-
+"""
 
---
+++ data_prep/test.sh
@@ -0,0 +1,1 @@
+python3 standardize_place_names.py --home_directory $HOME_DIR --geo_directory $GEO_DIR --data_directory $RES_DIR --file_name res_CHICAGO_2017.csv --gcp 
