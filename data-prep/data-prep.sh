dae462502ad71ec9e6a80a463283d0af59e3e6bf
chrisolen1 <chrisolen1@gmail.com>
2020-08-31 10:33:46 -0500

    asdf

--- data-prep/data-prep.sh
+++ data-prep/data-prep.sh
@@ -1,4 +1,4 @@
 python3 main.py \
 	--home_directory $HOME_DIR --geo_directory $GEO_DIR \
-	--data_directory $DATA_DIR --n_spark_workers 4 \
-	--n_processes 32 --gcp 
+	--data_directory $DATA_DIR --n_spark_workers 2 \
+	--n_processes 16 --gcp 
