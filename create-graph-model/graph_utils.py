12a5bdfc86df58640888647af835abfe4e7e8882
chrisolen1 <chrisolen1@gmail.com>
2020-09-02 18:00:06 -0500

    asdf

--- create-graph-model/graph_utils.py
+++ create-graph-model/graph_utils.py
@@ -171,7 +171,10 @@ class graph_model(object):
 			neo = blob.download_as_string().decode("utf-8").split('\n')[:-1]
 			os.system("kubectl exec -it neo4j-ce-1-0 -- cypher-shell -u 'neo4j' -p 'asdf' -d 'neo4j' --format plain 'MATCH (n) DETACH DELETE n'")
 			print("existing graph deleted")
-			result_list = self.run_imap_multiprocessing(func=self.neo_query, argument_list=neo, num_processes=8)
+			print(type(neo))
+			print(len(neo))
+			print(neo)
+			result_list = self.run_imap_multiprocessing(func=self.neo_query, argument_list=neo, num_processes=4)
 						
 
 		else:	
